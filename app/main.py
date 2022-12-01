ea7ab94b879c8d80c5d0194ee7b9db09d8c8b07b
cristianbl <crblanaru@gmail.com>
2022-12-01 13:54:27 +0100

    fixed confirmaps altered main

--- app/main.py
+++ app/main.py
@@ -10,5 +10,5 @@ def hello_world():
     api_key = os.getenv('API_KEY')
     db_endpoint = os.getenv('DBENDPOINT')
     my_rand = random.randint(100,999)
-    #return {"bunnyshell-neo": "is_amazing", "api_key": api_key, "db_endpoints": db_endpoint}
-    return {"bunnyshell-neo": "is_amazing", "api_key": api_key, "random": my_rand}
+    return {"bunnyshell-neo": "is_amazing", "api_key": api_key}
+    #return {"bunnyshell-neo": "is_amazing", "api_key": api_key, "random": my_rand}
